5f2082c999d8608973594c1094a69645eefa9522
Jarosław Leśniak <jarek9244@gmail.com>
2019-08-05 16:05:16 +0200

    Added PWA

---
+++ frontend/sw.js
@@ -0,0 +1,29 @@
+const cacheName = 'static';
+const assets = [
+    "./",
+    "./index.html",
+    "./css/app.css",
+    "./js/app.js",
+    "./img/icons/favicon.png",
+    "./manifest.json"
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            cache.addAll(assets);
+        })
+    );
+});
+
+self.addEventListener('activate', e => {
+    
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(cacheResource => {
+            return cacheResource || fetch(e.request);
+        })
+    );
+})
